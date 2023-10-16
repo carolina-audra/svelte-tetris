bfdaee57876f209a2bd1034e9aface4f374ec761
Carolina Audra <carolina.audra@hotmail.com>
2023-10-16 18:12:57 -0300

    Pieces and movement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,11 +25,173 @@ function coordsFromIndex(index: number) {
     return [column, row];
 }
 
+function getCell(x: number, y: number) {
+    return cells[coords(x, y)];
+}
+function setCell(x: number, y: number, value: CellStatus) {
+    cells[coords(x, y)] = value;
+}
 const cells = new Array<CellStatus>(cellsQty).fill(CellStatus.Empty);
-cells[coords(5, 0)] = CellStatus.Active;
-cells[coords(5, 1)] = CellStatus.Active;
-cells[coords(5, 2)] = CellStatus.Active;
-cells[coords(4, 2)] = CellStatus.Active;
+
+
+const pieces = [
+    [ // I piece
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0]
+        ]
+    ],
+    [ // O piece
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ],
+    [ // J piece
+        [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 1]
+        ],
+        [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
+        ],
+        [
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+        [
+            [0, 1, 1],
+            [0, 1, 0],
+            [0, 1, 0]
+        ]
+    ],
+    [ // L piece
+        [
+            [0, 0, 0],
+            [1, 1, 1],
+            [1, 0, 0]
+        ],
+        [
+            [1, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0]
+        ],
+        [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+        [
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1]
+        ]
+    ],
+    [ // S piece
+        [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 1, 0]
+        ],
+        [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 0, 1]
+        ],
+        [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 1, 0]
+        ],
+        [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 0, 1]
+        ]
+    ],
+    [ // T piece
+        [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0]
+        ],
+        [
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 1, 0]
+        ],
+        [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+        [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 1, 0]
+        ]
+    ],
+    [ // Z piece
+        [
+            [0, 0, 0],
+            [1, 1, 0],
+            [0, 1, 1]
+        ],
+        [
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 1, 0]
+        ],
+        [
+            [0, 0, 0],
+            [1, 1, 0],
+            [0, 1, 1]
+        ],
+        [
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 1, 0]
+        ]
+    ]
+];
+
+
+let latestPieceIndex = 0;
+let latestPieceRotation = 0;
+let latestPieceX = 0;
+let latestPieceY = 0;
+
+function initWithRandomPiece() {
+    cells.fill(CellStatus.Empty);
+    latestPieceIndex = Math.floor(Math.random() * pieces.length);
+    latestPieceRotation = Math.floor(Math.random() * pieces[latestPieceIndex].length);
+    const pieceByPos = pieces[latestPieceIndex][latestPieceRotation];
+    
+    const centerX = Math.floor(colsQty / 2 - (pieceByPos[0].length / 2));
+    latestPieceX = centerX;
+
+    for (let y = 0; y < pieceByPos.length; y++) {
+        for (let x = 0; x < pieceByPos[y].length; x++) {
+
+            if (pieceByPos[y][x]) setCell(centerX + x, y, CellStatus.Active);
+        }
+    }
+}
+initWithRandomPiece();
 
 function handleKeyDown(event: KeyboardEvent) {
 
@@ -49,9 +211,17 @@ function handleKeyDown(event: KeyboardEvent) {
             const [column, row] = coordsFromIndex(activeCellIndex);
 
 
-            if (event.key === "ArrowRight" && coords(column + 1, row) === activeCellIndex) {
-                movementBlocked = true;
-            } else if (event.key === "ArrowLeft" && coords(column - 1, row) === activeCellIndex) {
+            let desiredColumn = column;
+            if (event.key === "ArrowRight") {
+                desiredColumn = column + 1;
+            } else { // ArrowLeft
+                desiredColumn = column - 1;
+            }
+
+            if (
+                coords(desiredColumn, row) === activeCellIndex
+                || getCell(desiredColumn, row) === CellStatus.Fixed
+            ) {
                 movementBlocked = true;
             }
         }
@@ -60,18 +230,25 @@ function handleKeyDown(event: KeyboardEvent) {
             for (const activeCellIndex of activeCellsIndex) {
                 cells[activeCellIndex] = CellStatus.Empty;
             }
+            if (event.key === "ArrowRight") {
+                latestPieceX++;
+            } else { // ArrowLeft
+                latestPieceX--;
+            }
 
             for (const activeCellIndex of activeCellsIndex) {
                 const [column, row] = coordsFromIndex(activeCellIndex);
 
-            if (event.key === "ArrowRight") {
-                cells[coords(column + 1, row)] = CellStatus.Active;
+                if (event.key === "ArrowRight") {
+                setCell(column + 1, row, CellStatus.Active);
 
             } else { // ArrowLeft
-                cells[coords(column - 1, row)] = CellStatus.Active;
+                setCell(column - 1, row, CellStatus.Active);
             }
         }
         }
+    } else if (event.key === " ") {
+
     }
 }
 </script>
@@ -104,6 +281,11 @@ function handleKeyDown(event: KeyboardEvent) {
         {/each}
     </div>
 </div>
+
+<button on:click={initWithRandomPiece} type="button">Reset</button><br>
+Piece index: {latestPieceIndex}<br>
+Piece X: { latestPieceX }<br> 
+Rotation: { latestPieceIndex }<br>
 
 <svelte:window
     on:keydown={handleKeyDown}
